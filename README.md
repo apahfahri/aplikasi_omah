80b1528518845376d7a5726153358346f536aeba
apahfahri <ahmadfahrie19@gmail.com>
2024-12-30 19:47:41 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# aplikasi_omah
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/aplikasi_omah/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aplikasi_omah
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
