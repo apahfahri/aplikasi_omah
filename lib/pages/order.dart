e0f072024de59cd37f01ddcba2921ea933ee6e39
apahfahri <ahmadfahrie19@gmail.com>
2025-01-08 00:56:49 +0700

    perbaikan form halaman order

--- lib/pages/order.dart
+++ lib/pages/order.dart
@@ -1,6 +1,10 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:aplikasi_omah/pages/konfirmasi.dart';
+import 'package:aplikasi_omah/util/validator.dart';
+import 'package:aplikasi_omah/util/ETTER/model/pesanan_model.dart';
+import 'package:aplikasi_omah/util/ETTER/restapi/restapi.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -8,7 +12,7 @@ class Order extends StatefulWidget {
   final User user;
   final String pilihan;
 
-  Order({super.key, required this.user, required this.pilihan});
+  const Order({super.key, required this.user, required this.pilihan});
 
   @override
   OrderState createState() => OrderState();
@@ -18,23 +22,35 @@ class OrderState extends State<Order> {
   late User currentUser = widget.user;
   late String pilihan = widget.pilihan;
 
+  DataService ds = DataService();
+
   late Future<DateTime?> selectedDate;
   late Future<TimeOfDay?> selectedHour;
   String date = "-";
 
-  final nama_pelanggan = TextEditingController();
+  late final TextEditingController nama_pelanggan;
   final no_telpon = TextEditingController();
   final alamat = TextEditingController();
+  int jumlah_layanan = 1;
   final tgl_penjemputan = TextEditingController();
   var jam_penjemputan = TextEditingController();
   String pilihan_pembayaran = 'COD';
+
+  // Key for form validation
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize nama_pelanggan with user display name or empty if null
+    nama_pelanggan = TextEditingController(text: widget.user.displayName ?? '');
+  }
 
   @override
   Widget build(BuildContext context) {
     ThemeData.dark(useMaterial3: true);
     return Scaffold(
-      backgroundColor:
-          const Color(0xFFF0F4FF), // Warna latar belakang biru muda
+      backgroundColor: const Color(0xFFF0F4FF), // Warna latar belakang biru muda
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.transparent,
@@ -49,149 +65,199 @@ class OrderState extends State<Order> {
           style: TextStyle(color: Colors.black),
         ),
         centerTitle: false,
-        // actions: [
-        //   IconButton(
-        //     icon: const Icon(Icons.add, color: Colors.black),
-        //     onPressed: () {},
-        //   ),
-        // ],
       ),
       body: SingleChildScrollView(
         child: Container(
           alignment: Alignment.center,
           color: Colors.lightBlueAccent[50],
-          child: Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.all(20),
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.grey,
-                      spreadRadius: 1,
-                      blurRadius: 3,
-                      offset: Offset(0, 3),
-                    ),
-                  ],
+          child: Form(
+            key: _formKey, // Assign the form key
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(20),
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.grey,
+                        spreadRadius: 1,
+                        blurRadius: 3,
+                        offset: Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      buildTextFormField('Nama Pelanggan', nama_pelanggan, TextInputType.text),
+                      buildTextFormField('Nomor Telepon', no_telpon, TextInputType.phone),
+                      buildTextFormField('Alamat', alamat, TextInputType.streetAddress),
+                      buildNumberField('Jumlah Layanan', jumlah_layanan),
+                      buildTextFormField('Tanggal Penjemputan', tgl_penjemputan, TextInputType.datetime,  onTap: () => showDialogPicker(context)),
+                      buildTextFormField('Jam Penjemputan', jam_penjemputan, TextInputType.text,  onTap: () => dialogPickerClock(context)),
+                      buildPaymentDropdown(),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: _submitForm,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green[900],
+                        ),
+                        child: const Text(
+                          'Pesan',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 8, vertical: 16),
-                      child: TextField(
-                        controller: nama_pelanggan..text = currentUser.displayName ?? '',
-                        keyboardType: TextInputType.text,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          // hintText: "${currentUser.displayName}",
-                          labelText: 'Nama Pelanggan',
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 8, vertical: 16),
-                      child: TextField(
-                        controller: no_telpon,
-                        keyboardType: TextInputType.text,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Nomor Telepon',
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 8, vertical: 16),
-                      child: TextField(
-                        controller: alamat,
-                        keyboardType: TextInputType.text,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Alamat',
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 8, vertical: 16),
-                      child: TextField(
-                        controller: tgl_penjemputan,
-                        keyboardType: TextInputType.text,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Tanggal Penjemputan',
-                        ),
-                        onTap: () {
-                          showDialogPicker(context);
-                        },
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 8, vertical: 16),
-                      child: TextField(
-                        controller: jam_penjemputan,
-                        keyboardType: TextInputType.text,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Jam Penjemputan',
-                        ),
-                        onTap: () {
-                          dialogPickerClock(context);
-                        },
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 8, vertical: 16),
-                      child: DropdownButtonFormField(
-                        decoration: const InputDecoration(
-                          filled: false,
-                          // border: InputBorder.none,
-                          border: OutlineInputBorder(),
-                          labelText: 'Metode Pembayaran',
-                        ),
-                        value: pilihan_pembayaran,
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            pilihan_pembayaran = newValue!;
-                          });
-                        },
-                        items: <String>['COD', 'e-Money', 'Transefer Bank']
-                            .map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                      ),
-                    ),
-                    // const TextField(
-                    //   decoration: InputDecoration(
-                    //     // hintText: 'Catatan',
-                    //     labelText: 'Catatan',
-                    //   ),
-                    // ),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: const Text('Simpan'),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
+  // Reusable text field widget
+  Widget buildTextFormField(String label, TextEditingController controller, TextInputType keyboardType, {VoidCallback? onTap}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+      child: TextFormField(
+        validator: (controler) {
+          switch (label) {
+            case 'Nama Pelanggan':
+              return Validator.validateName(name: controler);
+            case 'Nomor Telepon':
+              return Validator.validatePhone(phone: controler);
+            case 'Alamat':
+              return Validator.validateEmpty(name: controler);
+            case 'Tanggal Penjemputan':
+              return Validator.validateEmpty(name: controler);
+            case 'Jam Penjemputan':
+              return Validator.validateEmpty(name: controler);
+            default:
+              return null;
+          }
+        },
+        controller: controller,
+        keyboardType: keyboardType,
+        decoration: InputDecoration(
+          border: const OutlineInputBorder(),
+          labelText: label,
+          errorBorder: UnderlineInputBorder(
+            borderRadius: BorderRadius.circular(8.5),
+            borderSide: const BorderSide(color: Colors.red),
+          ),
+        ),
+        onTap: onTap,
+      ),
+    );
+  }
+
+  // Reusable number input field
+  Widget buildNumberField(String label, int initialValue) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+      child: TextField(
+        controller: TextEditingController(text: initialValue.toString()),
+        keyboardType: TextInputType.number,
+        decoration: InputDecoration(
+          border: const OutlineInputBorder(),
+          labelText: label,
+          errorBorder: UnderlineInputBorder(
+            borderRadius: BorderRadius.circular(8.5),
+            borderSide: const BorderSide(color: Colors.red),
+          ),
+        ),
+        onChanged: (value) {
+          setState(() {
+            jumlah_layanan = int.tryParse(value) ?? 1;
+          });
+        },
+      ),
+    );
+  }
+
+  // Payment method dropdown
+  Widget buildPaymentDropdown() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+      child: DropdownButtonFormField<String>(
+        decoration: InputDecoration(
+          filled: false,
+          border: const OutlineInputBorder(),
+          labelText: 'Metode Pembayaran',
+          errorBorder: UnderlineInputBorder(
+            borderRadius: BorderRadius.circular(8.5),
+            borderSide: const BorderSide(color: Colors.red),
+          ),
+        ),
+        value: pilihan_pembayaran,
+        onChanged: (String? newValue) {
+          setState(() {
+            pilihan_pembayaran = newValue!;
+          });
+        },
+        items: <String>['COD', 'e-Money', 'Transfer Bank']
+            .map<DropdownMenuItem<String>>((String value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(value),
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+  // Submit form and navigate to the next page
+  void _submitForm() async {
+    if (_formKey.currentState!.validate()) {
+      List<PesananModel> pesanan = [
+        PesananModel(
+          id: '-',
+          no: 0,
+          pelanggan: nama_pelanggan.text,
+          no_telpon: no_telpon.text,
+          alamat: alamat.text,
+          jenis_layanan: pilihan,
+          jumlah_layanan: jumlah_layanan,
+          jam_penjemputan: jam_penjemputan.text,
+          tgl_penjemputan: tgl_penjemputan.text,
+          tgl_pengantaran: '-',
+          status_pesanan: 'Pesanan Baru',
+          metode_pembayaran: pilihan_pembayaran,
+          status_pembayaran: 'Belum Bayar',
+          total_harga: 0,
+          nama_kurir: '-',
+        ),
+      ];
+
+      if (pesanan.isNotEmpty) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => Konfirmasi(pesanan: pesanan[0]),
+          ),
+        );
+      }
+
+      // ScaffoldMessenger.of(context).showSnackBar(
+      //   const SnackBar(
+      //     content: Text('Data berhasil diinput'),
+      //   ),
+      // );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Data tidak boleh kosong'),
+        ),
+      );
+    }
+  }
+
+  // Time picker dialog
   void dialogPickerClock(BuildContext context) {
     selectedHour = showTimePicker(
       context: context,
@@ -207,49 +273,24 @@ class OrderState extends State<Order> {
     });
   }
 
+  // Date picker dialog
   void showDialogPicker(BuildContext context) {
     selectedDate = showDatePicker(
       context: context,
-      // initialDate: DateTime(date.year - 20, date.month, date.day),
       initialDate: DateTime.now(),
       firstDate: DateTime(1980),
       lastDate: DateTime(DateTime.now().year + 20),
-      // lastDate: option == 1 ? DateTime.now() : DateTime(2100),
-      builder: (BuildContext context, Widget? child) {
-        return Theme(
-          data: ThemeData.light(),
-          child: child!,
-        );
-      },
     );
 
     selectedDate.then((value) {
       setState(() {
         if (value == null) return;
 
-        if (kDebugMode) {
-          print('Line 230');
-        }
-
         final DateFormat formatter = DateFormat('dd-MMM-yyyy');
         final String formattedDate = formatter.format(value);
 
         tgl_penjemputan.text = formattedDate;
-        // if (option == 1) {
-        //   tglDibuat.text = formattedDate;
-        // } else {
-        //   tglDimulai.text = formattedDate;
-        // }
       });
-
-      if (kDebugMode) {
-        print('Line 240');
-      }
-    }, onError: (error) {
-      if (kDebugMode) {
-        print('Line 244');
-        print(error);
-      }
     });
   }
 }
--- lib/util/validator.dart
+++ lib/util/validator.dart
@@ -1,41 +1,62 @@
 class Validator {
-  static String? validateName({required String? name}){
+  static String? validateEmpty({required String? name}) {
     if (name == null) {
       return null;
     }
 
     if (name.isEmpty) {
-      return 'Name can\'t be empty';
+      return 'Kolom tidak boleh kosong';
     }
     return null;
   }
 
-  static String? validateEmail({required String? email}){
+  static String? validatePhone({required String? phone}) {
+    if (phone == null || phone.isEmpty) {
+      return 'Angka tidak boleh kosong';
+    }
+    if (!RegExp(r'^[0-9]+$').hasMatch(phone)) {
+      return 'Masukkan angka yang valid';
+    }
+    return null;
+  }
+
+  static String? validateName({required String? name}) {
+    if (name == null) {
+      return null;
+    }
+
+    if (name.isEmpty) {
+      return 'Nama tidak boleh kosong';
+    }
+    return null;
+  }
+
+  static String? validateEmail({required String? email}) {
     if (email == null) {
       return null;
     }
 
-    RegExp emailRegExp = RegExp(r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z09-]{0,253}[a-zA-Z0-9])?)*$");
+    RegExp emailRegExp = RegExp(
+        r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z09-]{0,253}[a-zA-Z0-9])?)*$");
 
     if (email.isEmpty) {
-      return 'Email can\'t be empty';
-    }else if(!emailRegExp.hasMatch(email)){
-        return 'Enter a correct email';
+      return 'Email tidak boleh kosong';
+    } else if (!emailRegExp.hasMatch(email)) {
+      return 'masukkan email yang valid';
     }
     return null;
   }
 
-  static String? validatePassword({required String? password}){
+  static String? validatePassword({required String? password}) {
     if (password == null) {
       return null;
     }
 
     if (password.isEmpty) {
-      return 'Name can\'t be empty';
-    }else if(password.length<6){
-        return 'Enter a password with lenght at least 6';
+      return 'Password tidak boleh kosong';
+    } else if (password.length < 6) {
+      return 'masukkan password minimal 6 karakter';
     }
     return null;
   }
-
 }
